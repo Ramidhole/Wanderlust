6dc5fa10776a9b060e66b3b623e43fc0d204b603
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2024-12-19 15:36:39 +0530

    add data

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/Wanderlust";
+
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+
+    .catch((err) => {
+        console.log(err);
+
+    });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
 app.get("/",(req,res)=>{
     res.send("Hi, i am root");
+});
+
+app.get("/testListing",async(req,res)=>{
+let sampleListing = new Listing({
+    title:"My New Villa",
+    description:"By the beach",
+    price:13000,
+    location:"Calangute,Goa",
+    country:"India",
+});
+await sampleListing.save();
+console.log("sample was saved");
+    res.send("succesfull testing");
+
 });
 
 app.listen(7000,()=>{
---
+++ models/listing.js
@@ -0,0 +1,31 @@
+// const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    
+    description:String,
+
+    image:{
+        type:String,
+        default: "https://unsplash.com/photos/a-foggy-forest-filled-with-lots-of-trees-43BbfNokxJE",
+
+        set:(v)=> 
+            v===""
+                ?"https://unsplash.com/photos/a-foggy-forest-filled-with-lots-of-trees-43BbfNokxJE"
+                :v,
+    },
+    
+    price:Number,
+    location:String,
+    country:String,
+
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
